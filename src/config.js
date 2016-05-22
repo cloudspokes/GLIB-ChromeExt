b0f23d71fca5b2a79171fbbed9cb609464a3fe44
CWD <cwd@cloudspokes.com>
2016-05-21 21:54:21 -0400

    fixing jwt token handling

--- src/config.js
+++ src/config.js
@@ -9,21 +9,21 @@
  */
 
 
-var OAUTH_API_KEY = '8XdfQwEhyOpybXm-usraorw483U';
+var OAUTH_API_KEY = 'MyM7bhB25wdkNKI2OCa6137E1u8';
 
 var TC_ENDPOINT_DEV = 'https://glib-mock.herokuapp.com/';
 var TC_ENDPOINT_PROD = 'https://glib-prod.herokuapp.com/';
 var EXTENSION_ID = 'kbdpmophclfhglceikdgbcoambjjgkgb';
 
 var OAUTH_APPS = {
-  gitlab: {
-    clientId: '2e7ddb0f2135d5f81407d49941a6cb1a83767b200a584e97f9fb92684ce71c89',
-    clientSecret: '79d574243aa4a133db7ac16b37a994c8698ee22708d24a1bf2a0a5c16486b293',
-    redirectUri: `https://${EXTENSION_ID}.chromiumapp.org/oauth2`,
-    scope: 'api',
-    authorizeUrl: 'https://gitlab.com/oauth/authorize',
-    tokenUrl: 'https://gitlab.com/oauth/token'
-  }
+    gitlab: {
+        clientId: '2e7ddb0f2135d5f81407d49941a6cb1a83767b200a584e97f9fb92684ce71c89',
+        clientSecret: '79d574243aa4a133db7ac16b37a994c8698ee22708d24a1bf2a0a5c16486b293',
+        redirectUri: `https://${EXTENSION_ID}.chromiumapp.org/oauth2`,
+        scope: 'api',
+        authorizeUrl: 'https://gitlab.com/oauth/authorize',
+        tokenUrl: 'https://gitlab.com/oauth/token'
+    }
 };
 
 // Shared constants, do not change
@@ -44,4 +44,3 @@ var DEFAULT_TC_OAUTH_REDIRECT_URI = 'https://kbdpmophclfhglceikdgbcoambjjgkgb.ch
 var DEFAULT_TC_OAUTH_URL = 'https://accounts.topcoder-dev.com/oauth';
 
 var ENVIRONMENT = 'glib::environment';
-
--- src/options.js
+++ src/options.js
@@ -29,16 +29,16 @@ var VALIDATE_TIMEOUT = 1000; // show validation error 1s after key up
  * Set the value in Chrome Storage
  */
 function setChromeStorage(key, val) {
-  var obj = {};
-  obj[key] = val;
-  chrome.storage.local.set(obj);
+    var obj = {};
+    obj[key] = val;
+    chrome.storage.local.set(obj);
 }
 
 /**
  * Remove a key from Chrome Storage
  */
 function removeChromeStorage(key) {
-  chrome.storage.local.remove(key);
+    chrome.storage.local.remove(key);
 }
 
 /**
@@ -49,264 +49,264 @@ function removeChromeStorage(key) {
  * @param defaultDomain the default domain (placeholder value)
  */
 function _initDomainField(selector, key, defaultDomain) {
-  const $input = $(selector);
-  const $reset = $input.next('.reset');
-  const $error = $('<div class="error">Invalid domain</div>');
-  $input.parent().append($error);
-  $error.hide();
-  let timeoutId;
-  $input.attr('placeholder', defaultDomain);
+    const $input = $(selector);
+    const $reset = $input.next('.reset');
+    const $error = $('<div class="error">Invalid domain</div>');
+    $input.parent().append($error);
+    $error.hide();
+    let timeoutId;
+    $input.attr('placeholder', defaultDomain);
 
-  // show/hide error state
-  const toggleError = (isValid) => {
-    if (isValid) {
-      $input.removeClass('is-error');
-      $error.hide();
-    } else {
-      $input.addClass('is-error');
-      $error.show();
-    }
-  };
-
-  // load value from local storage
-  chrome.storage.local.get(key, function (result) {
-    const value = result[key];
-    if (value) {
-      $input.val(value);
-    } else {
-      $reset.prop('disabled', true);
-    }
-  });
-
-  // handle input change
-  // save and validate
-  $input.keyup(() => {
-    const value = $input.val();
-    let isValid = true;
-    if (value && value.trim().length) {
-      isValid = DOMAIN_REGEX.test(value);
-      if (isValid) {
-        setChromeStorage(key, value);
-      }
-      $reset.prop('disabled', false);
-    } else {
-      removeChromeStorage(key);
-      $reset.prop('disabled', true);
-    }
-    clearTimeout(timeoutId);
-    toggleError(true);
-    timeoutId = setTimeout(() => {
-      toggleError(isValid);
-    }, VALIDATE_TIMEOUT);
-  });
-
-  // reset value
-  $reset.click(() => {
-    vex.dialog.confirm({
-      message: 'Are you sure to reset this domain ?',
-      callback: function (value) {
-        if (!value) {
-          return;
+    // show/hide error state
+    const toggleError = (isValid) => {
+        if (isValid) {
+            $input.removeClass('is-error');
+            $error.hide();
+        } else {
+            $input.addClass('is-error');
+            $error.show();
         }
-        removeChromeStorage(key);
-        $reset.prop('disabled', true);
-        toggleError(true);
-        $input.val('');
-      }
+    };
+
+    // load value from local storage
+    chrome.storage.local.get(key, function(result) {
+        const value = result[key];
+        if (value) {
+            $input.val(value);
+        } else {
+            $reset.prop('disabled', true);
+        }
     });
-  });
+
+    // handle input change
+    // save and validate
+    $input.keyup(() => {
+        const value = $input.val();
+        let isValid = true;
+        if (value && value.trim().length) {
+            isValid = DOMAIN_REGEX.test(value);
+            if (isValid) {
+                setChromeStorage(key, value);
+            }
+            $reset.prop('disabled', false);
+        } else {
+            removeChromeStorage(key);
+            $reset.prop('disabled', true);
+        }
+        clearTimeout(timeoutId);
+        toggleError(true);
+        timeoutId = setTimeout(() => {
+            toggleError(isValid);
+        }, VALIDATE_TIMEOUT);
+    });
+
+    // reset value
+    $reset.click(() => {
+        vex.dialog.confirm({
+            message: 'Are you sure to reset this domain ?',
+            callback: function(value) {
+                if (!value) {
+                    return;
+                }
+                removeChromeStorage(key);
+                $reset.prop('disabled', true);
+                toggleError(true);
+                $input.val('');
+            }
+        });
+    });
 }
 
-$(document).ready(function () {
-  _initDomainField('#githubDomain', DOMAIN_KEY_GITHUB, DEFAULT_GITHUB_DOMAIN);
-  _initDomainField('#gitlabDomain', DOMAIN_KEY_GITLAB, DEFAULT_GITLAB_DOMAIN);
-  _initDomainField('#jiraDomain', DOMAIN_KEY_JIRA, DEFAULT_JIRA_DOMAIN);
+$(document).ready(function() {
+    _initDomainField('#githubDomain', DOMAIN_KEY_GITHUB, DEFAULT_GITHUB_DOMAIN);
+    _initDomainField('#gitlabDomain', DOMAIN_KEY_GITLAB, DEFAULT_GITLAB_DOMAIN);
+    _initDomainField('#jiraDomain', DOMAIN_KEY_JIRA, DEFAULT_JIRA_DOMAIN);
 
     /* initialization for vex library */
-  vex.defaultOptions.className = 'vex-theme-os';
+    vex.defaultOptions.className = 'vex-theme-os';
     /* Onload populate mappings from Chrome Storage */
-  populateRepoMaps();
+    populateRepoMaps();
 
     /* Populate OAuth tokens */
-  chrome.storage.local.get(TOKEN_KEY_GITHUB, function (result) {
-    if (result[TOKEN_KEY_GITHUB] == undefined) {
-      $(".delete[type='github']").prop('disabled', true);
-    } else {
-      $('#githubToken').val(result[TOKEN_KEY_GITHUB]);
-    }
-  });
-  chrome.storage.local.get(TOKEN_KEY_GITLAB, function (result) {
-    if (result[TOKEN_KEY_GITLAB] == undefined) {
-      $(".delete[type='gitlab']").prop('disabled', true);
-    } else {
-      $('#gitlabToken').val(result[TOKEN_KEY_GITLAB]);
-    }
-  });
-  chrome.storage.local.get(TC_OAUTH_TOKEN_KEY, function (result) {
-    if (result[TC_OAUTH_TOKEN_KEY] == undefined) {
-      $(".delete[type='topcoder']").prop('disabled', true);
-    } else {
-      $('#topCoderToken').val(JSON.stringify(result[TC_OAUTH_TOKEN_KEY]));
-    }
-  });
+    chrome.storage.local.get(TOKEN_KEY_GITHUB, function(result) {
+        if (result[TOKEN_KEY_GITHUB] == undefined) {
+            $(".delete[type='github']").prop('disabled', true);
+        } else {
+            $('#githubToken').val(result[TOKEN_KEY_GITHUB]);
+        }
+    });
+    chrome.storage.local.get(TOKEN_KEY_GITLAB, function(result) {
+        if (result[TOKEN_KEY_GITLAB] == undefined) {
+            $(".delete[type='gitlab']").prop('disabled', true);
+        } else {
+            $('#gitlabToken').val(result[TOKEN_KEY_GITLAB]);
+        }
+    });
+    chrome.storage.local.get(TC_OAUTH_TOKEN_KEY, function(result) {
+        if (result[TC_OAUTH_TOKEN_KEY] == undefined) {
+            $(".delete[type='topcoder']").prop('disabled', true);
+        } else {
+            $('#topCoderToken').val(result[TC_OAUTH_TOKEN_KEY]);
+        }
+    });
 
     // CWD-- set checkbox status
-  chrome.storage.local.get(ENVIRONMENT, function (result) {
-    if (result[ENVIRONMENT] == undefined) {
-      $('#environment').prop('checked', false);
-    } else {
-      $('#environment').prop('checked', result[ENVIRONMENT]);
-    }
-  });
+    chrome.storage.local.get(ENVIRONMENT, function(result) {
+        if (result[ENVIRONMENT] == undefined) {
+            $('#environment').prop('checked', false);
+        } else {
+            $('#environment').prop('checked', result[ENVIRONMENT]);
+        }
+    });
 
     // CWD-- bind event on checkbox
-  $('#environment').click(function () {
-    setChromeStorage(ENVIRONMENT, $('#environment').prop('checked'));
-  });
+    $('#environment').click(function() {
+        setChromeStorage(ENVIRONMENT, $('#environment').prop('checked'));
+    });
 
     /* Delete GitHub and TopCoder tokens */
-  $('.delete:not(.reset)').click(function () {
-    var self = this;
-    vex.dialog.confirm({
-      message: 'Are you sure to delete the token ?',
-      callback: function (value) {
-        if (value) {
-          if ($(self).attr('type') == 'github') {
-            $(".delete[type='github']").prop('disabled', true);
-            removeChromeStorage(TOKEN_KEY_GITHUB);
-            $('#githubToken').val('');
-          } else if ($(self).attr('type') == 'gitlab') {
-            $(".delete[type='gitlab']").prop('disabled', true);
-            removeChromeStorage(TOKEN_KEY_GITLAB);
-            $('#gitlabToken').val('');
-          } else {
-            $(".delete[type='topcoder']").prop('disabled', true);
-            removeChromeStorage(TC_OAUTH_TOKEN_KEY);
-            $('#topCoderToken').val('');
-          }
-        }
-      }
+    $('.delete:not(.reset)').click(function() {
+        var self = this;
+        vex.dialog.confirm({
+            message: 'Are you sure to delete the token ?',
+            callback: function(value) {
+                if (value) {
+                    if ($(self).attr('type') == 'github') {
+                        $(".delete[type='github']").prop('disabled', true);
+                        removeChromeStorage(TOKEN_KEY_GITHUB);
+                        $('#githubToken').val('');
+                    } else if ($(self).attr('type') == 'gitlab') {
+                        $(".delete[type='gitlab']").prop('disabled', true);
+                        removeChromeStorage(TOKEN_KEY_GITLAB);
+                        $('#gitlabToken').val('');
+                    } else {
+                        $(".delete[type='topcoder']").prop('disabled', true);
+                        removeChromeStorage(TC_OAUTH_TOKEN_KEY);
+                        $('#topCoderToken').val('');
+                    }
+                }
+            }
+        });
     });
-  });
 
     /* Delete mappings on click of delete icons */
-  $(document).on('click', '.remove', function (e) {
-    var index = $(this).attr('index');
-    var self = this;
-    chrome.storage.local.get('repoMap', function (result) {
+    $(document).on('click', '.remove', function(e) {
+        var index = $(this).attr('index');
+        var self = this;
+        chrome.storage.local.get('repoMap', function(result) {
             /* Remove a specfic index from the existing data array */
-      result.repoMap.splice(index, 1);
-      setChromeStorage('repoMap', result.repoMap);
-      $(self).parent().fadeOut(300, function () {
-        $(self).parent().remove();
-      });
+            result.repoMap.splice(index, 1);
+            setChromeStorage('repoMap', result.repoMap);
+            $(self).parent().fadeOut(300, function() {
+                $(self).parent().remove();
+            });
             /* Change each row's index after delete */
-      $('#mapWrapper div').each(function (index) {
-        $(this).find('.project-id,.repo-url,.remove').attr('index', index);
-      });
-      if ($('#mapWrapper div').length == 1) {
-        $('.delete-all').prop('disabled', true);
-      }
-    });
+            $('#mapWrapper div').each(function(index) {
+                $(this).find('.project-id,.repo-url,.remove').attr('index', index);
+            });
+            if ($('#mapWrapper div').length == 1) {
+                $('.delete-all').prop('disabled', true);
+            }
+        });
 
-  });
+    });
 
     /* Add a new mapping. Store the data in Chrome Storage. */
-  $('#add').click(function () {
-    chrome.storage.local.get('repoMap', function (result) {
-      var mapObj = {
-        'projectId': $('#pid').val(),
-        'repoURL': $('#repoUrl').val()
-      };
+    $('#add').click(function() {
+        chrome.storage.local.get('repoMap', function(result) {
+            var mapObj = {
+                'projectId': $('#pid').val(),
+                'repoURL': $('#repoUrl').val()
+            };
 
-      if (result.repoMap == undefined || result.repoMap.length == 0) {
-        setChromeStorage('repoMap', [mapObj]);
-      } else {
+            if (result.repoMap == undefined || result.repoMap.length == 0) {
+                setChromeStorage('repoMap', [mapObj]);
+            } else {
                 /* Push to existing data */
-        result.repoMap.push(mapObj);
-        setChromeStorage('repoMap', result.repoMap);
-      }
-      populateRepoMaps();
+                result.repoMap.push(mapObj);
+                setChromeStorage('repoMap', result.repoMap);
+            }
+            populateRepoMaps();
+        });
     });
-  });
 
     /* Delete all mappings. */
-  $('.delete-all').click(function () {
-    var self = this;
-    vex.dialog.confirm({
-      message: 'Are you sure to delete all mappings ?',
-      callback: function (value) {
-        if (value) {
-          removeChromeStorage('repoMap');
-          populateRepoMaps();
-        }
-      }
+    $('.delete-all').click(function() {
+        var self = this;
+        vex.dialog.confirm({
+            message: 'Are you sure to delete all mappings ?',
+            callback: function(value) {
+                if (value) {
+                    removeChromeStorage('repoMap');
+                    populateRepoMaps();
+                }
+            }
+        });
     });
-  });
 
     /**
      * Mass addition of mappings. The default separator is ###.
      * One mapping on each line
      */
-  $('#addMass').click(function () {
-    var self = this;
-    vex.dialog.open({
-      message: 'Enter TopCoder project id and GitHub repo separated by ' +
+    $('#addMass').click(function() {
+        var self = this;
+        vex.dialog.open({
+            message: 'Enter TopCoder project id and GitHub repo separated by ' +
                 ADD_MASS_DELIMETER + ' (one mapping on each line)',
-      input: '<textarea rows="7" placeholder="PROJECT_ID ' + ADD_MASS_DELIMETER +
+            input: '<textarea rows="7" placeholder="PROJECT_ID ' + ADD_MASS_DELIMETER +
                 ' REPO_URL\nPROJECT_ID ' + ADD_MASS_DELIMETER +
                 ' REPO_URL" name="mappings"></textarea>',
-      buttons: [
-        $.extend({}, vex.dialog.buttons.YES, {
-          text: 'Add mass'
-        }), $.extend({}, vex.dialog.buttons.NO, {
-          text: 'Cancel'
-        })
-      ],
-      callback: function (data) {
-        if (data === false) {
-          console.log('canceled');
-        } else {
-          var mapArr = [];
+            buttons: [
+                $.extend({}, vex.dialog.buttons.YES, {
+                    text: 'Add mass'
+                }), $.extend({}, vex.dialog.buttons.NO, {
+                    text: 'Cancel'
+                })
+            ],
+            callback: function(data) {
+                if (data === false) {
+                    console.log('canceled');
+                } else {
+                    var mapArr = [];
                     /* Split each new line */
-          var lines = data.mappings.split('\n');
-          for (var i = 0; i < lines.length; i++) {
-            var projectId = lines[i].split(ADD_MASS_DELIMETER)[0];
-            var gitRepo = lines[i].split(ADD_MASS_DELIMETER)[1];
-            mapArr.push({
-              'projectId': projectId,
-              'repoURL': gitRepo
-            });
-          }
+                    var lines = data.mappings.split('\n');
+                    for (var i = 0; i < lines.length; i++) {
+                        var projectId = lines[i].split(ADD_MASS_DELIMETER)[0];
+                        var gitRepo = lines[i].split(ADD_MASS_DELIMETER)[1];
+                        mapArr.push({
+                            'projectId': projectId,
+                            'repoURL': gitRepo
+                        });
+                    }
                     /* Read from Chrome Storage and push to existing array */
-          chrome.storage.local.get('repoMap', function (result) {
-            var mapsInStorage = result.repoMap;
-            if (mapsInStorage == undefined || mapsInStorage.length == 0) {
-              mapsInStorage = [];
-            }
+                    chrome.storage.local.get('repoMap', function(result) {
+                        var mapsInStorage = result.repoMap;
+                        if (mapsInStorage == undefined || mapsInStorage.length == 0) {
+                            mapsInStorage = [];
+                        }
                         /* Push to existing data */
-            for (var i = 0; i < mapArr.length; i++) {
-              mapsInStorage.push(mapArr[i]);
+                        for (var i = 0; i < mapArr.length; i++) {
+                            mapsInStorage.push(mapArr[i]);
+                        }
+                        setChromeStorage('repoMap', mapsInStorage);
+                        populateRepoMaps();
+                    });
+                }
             }
-            setChromeStorage('repoMap', mapsInStorage);
-            populateRepoMaps();
-          });
-        }
-      }
+        });
     });
-  });
 
     /* Mapping fields are editable. Save the data while user edits the fields */
-  $(document).on('keyup', '.project-id,.repo-url', function () {
-    var index = $(this).attr('index');
-    chrome.storage.local.get('repoMap', function (result) {
-      result.repoMap[index] = {
-        'projectId': $(".project-id[index='" + index + "']").val(),
-        'repoURL': $(".repo-url[index='" + index + "']").val()
-      };
-      setChromeStorage('repoMap', result.repoMap);
+    $(document).on('keyup', '.project-id,.repo-url', function() {
+        var index = $(this).attr('index');
+        chrome.storage.local.get('repoMap', function(result) {
+            result.repoMap[index] = {
+                'projectId': $(".project-id[index='" + index + "']").val(),
+                'repoURL': $(".repo-url[index='" + index + "']").val()
+            };
+            setChromeStorage('repoMap', result.repoMap);
+        });
     });
-  });
 });
 
 
@@ -314,27 +314,27 @@ $(document).ready(function () {
  * Read values from Chrome Storage and populate the mapping list
  */
 function populateRepoMaps() {
-  $('#mapWrapper').empty();
-  chrome.storage.local.get('repoMap', function (result) {
-    if (result.repoMap != undefined) {
-      if (result.repoMap == '' || result.repoMap.length == 0) {
-        $('.delete-all').prop('disabled', true);
-      } else {
-        $('.delete-all').prop('disabled', false);
-      }
+    $('#mapWrapper').empty();
+    chrome.storage.local.get('repoMap', function(result) {
+        if (result.repoMap != undefined) {
+            if (result.repoMap == '' || result.repoMap.length == 0) {
+                $('.delete-all').prop('disabled', true);
+            } else {
+                $('.delete-all').prop('disabled', false);
+            }
 
-      for (var i = 0; i < result.repoMap.length; i++) {
-        var html = "<input type='text' class='project-id' index=" + i + '>' +
+            for (var i = 0; i < result.repoMap.length; i++) {
+                var html = "<input type='text' class='project-id' index=" + i + '>' +
                     "<input type='text' class='repo-url' index=" + i + '>' +
                     "<img class='remove' src='images/delete.png' index=" + i + '>';
-        var div = $('<div>')
+                var div = $('<div>')
                     .appendTo('#mapWrapper')
                     .append(html);
-        $(div).find('.project-id').val(result.repoMap[i].projectId);
-        $(div).find('.repo-url').val(result.repoMap[i].repoURL);
-      }
-    } else {
-      $('.delete-all').prop('disabled', true);
-    }
-  });
+                $(div).find('.project-id').val(result.repoMap[i].projectId);
+                $(div).find('.repo-url').val(result.repoMap[i].repoURL);
+            }
+        } else {
+            $('.delete-all').prop('disabled', true);
+        }
+    });
 }
